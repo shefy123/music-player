0bd1d680165adf67849f22c1636c2c5397bd34aa
sarahsga <sarahsga4@gmail.com>
2018-06-09 20:45:50 +0500

    sarah changes

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 // first you create a arry for song which want to play
 
-var songs = ["cheap-thrills.mp3","amor.mp3","don,t.mp3"];
+var songs = ["cheap-thrills.mp3", "amor.mp3", "don,t.mp3"];
 // now create an arry of poster you want to use
-var poster = ["thills.jpg,amor.jpg,otelia.jpg"];
+var poster = ["thills.jpg", "amor.jpg", "otelia.jpg"];
 
 var songtitle = document.getElementById("songtitle");
 var fillbar = document.getElementById("fill");
 
 // now create an object for audio class
 
-var song = new Audio();
-var currentSong: 0;
+var song;
+var currentSong = 0;
 
 // now i want to play song when body or window is load
 
-window.onload = playSong;    
-function playSong ();
-    song.src = songs[currentSong];
+function playSomething() {
+    song = new Audio();
+    
+    song.src = "songs/" + songs[currentSong];
+    console.log(song.src);
     song.title.textContent = songs[currentSong];
     song.play();
+}
